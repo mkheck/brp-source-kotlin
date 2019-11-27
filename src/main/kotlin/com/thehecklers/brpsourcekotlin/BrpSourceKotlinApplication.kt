ba553db51b38c35661055b93831aa1f0193a0c3c
Mark Heckler <mark.heckler@gmail.com>
2019-11-27 17:34:37 -0600

    Moving to newer, functional API

--- src/main/kotlin/com/thehecklers/brpsourcekotlin/BrpSourceKotlinApplication.kt
+++ src/main/kotlin/com/thehecklers/brpsourcekotlin/BrpSourceKotlinApplication.kt
@@ -21,17 +21,17 @@ fun main(args: Array<String>) {
     runApplication<BrpSourceKotlinApplication>(*args)
 }
 
-//@Configuration
-//@EnableScheduling
-//class GateAgent(val generator: PassengerGenerator) {
-//    @Scheduled(fixedRate = 1000)
-//    @Bean
-//    fun sendPassenger(): () -> Passenger = {
-//        val pax = generator.generate()
-//		println(pax)
-//		pax
-//    }
-//}
+@Configuration
+@EnableScheduling
+class GateAgent(val generator: PassengerGenerator) {
+    @Scheduled(fixedRate = 1000)
+    @Bean
+    fun sendPassenger(): () -> Passenger = {
+        val pax = generator.generate()
+		println(pax)
+		pax
+    }
+}
 
 /*
 @Configuration
@@ -45,6 +45,7 @@ class GateAgent(val generator: PassengerGenerator) {
 }
 */
 
+/*
 @EnableBinding(Source::class)
 @EnableScheduling
 class GateAgent(private val source: Source, private val generator: PassengerGenerator) {
@@ -52,6 +53,7 @@ class GateAgent(private val source: Source, private val generator: PassengerGene
     fun sendPassenger() =
         source.output().send(MessageBuilder.withPayload(generator.generate()).build())
 }
+*/
 
 @Component
 class PassengerGenerator() {
