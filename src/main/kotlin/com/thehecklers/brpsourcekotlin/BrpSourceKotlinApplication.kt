12d80ec8d1773fd8f1d535c3be52c875fcdfc02a
Mark Heckler <mark.heckler@gmail.com>
2019-12-01 17:29:03 -0500

    Trying various idiomatic & non-idiomatic Kotlin approaches to providing a Supplier for SCSt

--- src/main/kotlin/com/thehecklers/brpsourcekotlin/BrpSourceKotlinApplication.kt
+++ src/main/kotlin/com/thehecklers/brpsourcekotlin/BrpSourceKotlinApplication.kt
@@ -23,30 +23,6 @@ fun main(args: Array<String>) {
     runApplication<BrpSourceKotlinApplication>(*args)
 }
 
-/*@Configuration
-@EnableScheduling
-class DeskAgent(val generator: PassengerGenerator) {
-    @Scheduled(fixedRate = 1000)
-    @Bean
-    fun sendPassenger(): () -> Passenger = {
-        val pax = generator.generate()
-		println(pax)
-		pax
-    }
-}*/
-
-/*
-@Configuration
-@EnableScheduling
-class GateAgent(val generator: PassengerGenerator) {
-	@Scheduled(fixedRate = 1000)
-	@Bean
-	fun sendPassenger(): Supplier<Passenger> {
-		return Supplier<Passenger> { generator.generate() }
-	}
-}
-*/
-
 /*
 @Configuration
 class GateAgent(private val generator: PassengerGenerator) {
@@ -60,12 +36,29 @@ class GateAgent(private val generator: PassengerGenerator) {
 */
 
 @EnableBinding(Source::class)
+class GateAgent(private val generator: PassengerGenerator) {
+    @Bean
+//    fun checkIn(): () -> Flux<Passenger> {
+//        return {
+        fun checkIn(): Supplier<Flux<Passenger>> {
+        return Supplier{
+            Flux.interval(Duration.ofSeconds(1))
+                .onBackpressureDrop()
+                .map { generator.generate() }
+        }
+    }
+}
+
+/*
+
+@EnableBinding(Source::class)
 @EnableScheduling
 class GateAgent(private val source: Source, private val generator: PassengerGenerator) {
     @Scheduled(fixedRate = 1000)
     fun sendPassenger() =
         source.output().send(MessageBuilder.withPayload(generator.generate()).build())
 }
+*/
 
 @Component
 class PassengerGenerator() {
